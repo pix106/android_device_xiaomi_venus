454578d780af4eeadcc16855ac85a0f4b0a2d5e4
Giovanni Ricca <giovanniricca@proton.me>
2022-09-11 22:08:03 +0200

    venus: Include device specific kernel config fragment
    Change-Id: Id5cacc4f1caf8edb3defc1f793a0c501039269f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,6 @@ DEVICE_PATH := device/xiaomi/venus
 
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
+
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/venus_QGKI.config
